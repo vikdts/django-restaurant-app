3e9a5d3febfbb4eaf156d48b699bbeccd39e4024
vikdts <>
2023-12-07 15:49:00 +0000

    create book model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    num_guests = models.PositiveIntegerField()
+    date = models.DateTimeField()
+    content = models.TextField()
