643ba8541a8effaa0ceff3c12ab666ad81a57784
vikdts <>
2024-01-07 13:55:27 +0000

    add date and time widget to form

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -2,9 +2,29 @@ from .models import Booking
 from django import forms
 from datetime import datetime
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
+time_format = [
+    '%H:%M'
+]
+
+date_format = [
+    '%Y-%m-%d'
+]
+
 
 class BookingForm(forms.Form):
     first_name = forms.CharField(label="First Name")
     last_name = forms.CharField(label="Last Name")
-    date = forms.DateField()
-    time = forms.TimeField()
+    date = forms.DateField(
+        widget=DateInput,
+        input_formats=date_format
+    )
+    time = forms.TimeField(
+        widget=TimeInput,
+        input_formats=time_format
+    )
