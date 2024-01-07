6e8c4d00e5074b70c42b50da18c06d80b85d48c0
vikdts <>
2024-01-07 13:49:05 +0000

    create booking view

--- restaurant/views.py
+++ restaurant/views.py
@@ -6,6 +6,16 @@ from .forms import BookingForm
 class BookingView(generic.View):
     template_name = 'book.html'
 
+    def get(self, request, *args, form=None, **kwargs):
+        form = form or BookingForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            return self.get(request)
+        return self.get(request, form=form)
+
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
 
